e710fbef80c57b2c1dc73afaf2e3f3f2c04f4e96
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2022-05-07 19:01:58 +0300

    task GroupWordsByCommonLetters.kt

---
+++ src/tasks/GroupWordsByCommonLetters.kt
@@ -0,0 +1,36 @@
+package tasks
+
+/**
+ *  @author Andrey Slesarchuk
+ *  @date 2022-05-07
+ */
+
+fun main() {
+    // group words by equal common letters
+    // input: ["eat", "tea", "ten", "ate", "tan", "nat", "bat"]
+    // result: [[eat, tea, ate], [ten], [tan, nat], [bat]]
+
+    println(groupWords(arrayOf("eat", "tea", "ten", "ate", "tan", "nat", "bat")))
+}
+
+fun groupWords(words: Array<String>): List<List<String>> {
+    val result: MutableList<List<String>> = mutableListOf()
+
+    val map = mutableMapOf<String, MutableList<String>>()
+
+    for (word in words) {
+        val sortedWord = word.toCharArray().sorted().joinToString("")
+
+        if (map.containsKey(sortedWord))
+            map[sortedWord]?.add(word)
+        else
+            map[sortedWord] = mutableListOf(word)
+    }
+
+
+    for (entity in map) {
+        result.add(entity.value)
+    }
+
+    return result
+}
