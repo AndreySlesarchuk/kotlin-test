f0139898039cca51d1d4c6d1bcc5fedb917b158e
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2022-07-07 18:34:34 +0300

    Loops.kt - code added

--- src/main/kotlin/by/atmm/kotlin/topics/Loops.kt
+++ src/main/kotlin/by/atmm/kotlin/topics/Loops.kt
@@ -6,6 +6,23 @@ package topics
  */
 
 fun main() {
+
+    val array = arrayOfNulls<Int?>(101)
+    for (i in 0 until array.size) array[i] = i
+    for ((index, i) in array.withIndex()) {
+        array[index] = i?.times(2)
+    }
+    for (i in array) print(i)
+    println()
+    println("-----------------------------------------------------------------")
+    val arr = arrayOfNulls<Int?>(301)
+    for ((index, i) in (300..600).withIndex()) {
+        arr[index] = i
+    }
+    for (i in arr.size - 1 downTo 0 step 5) print(i)
+    println()
+    println("-----------------------------------------------------------------")
+
     val items = listOf("apple", "banana", "orange") // List.of() Java 9
     val items2 = mutableListOf("carrot", "potato", "onion") // ArrayList
     items2.add("cucumber")
