040bb3e6d4b3324bd73e6f525633666cecc3f781
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2022-07-07 19:05:21 +0300

    Functions.kt - code added

--- src/main/kotlin/by/atmm/kotlin/topics/Functions.kt
+++ src/main/kotlin/by/atmm/kotlin/topics/Functions.kt
@@ -20,12 +20,54 @@ fun main() {
 
     printEven(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
     printEven(*intArrayOf(1, 2, 3, 4, 5), 6, 7, 8, 9, 10)
+
+    println("Max: ${max(11, 22)}")
+    println("Crop: ${crop("JavaKotlin")}")
+    println("Crop: ${crop("Java")}")
+    println("Sum: ${sum(1, 2, 3, 4, 5, 6, 7)}")
+
+
+    val numbers = mutableListOf<Int>(5, 8, 12, -3, 7, 6)  // arrayOf(5, 8, 12, -3, 7, 6)
+    var result = sort(numbers)
+    for (i in result) print(" $i")
+    result = sort(5, 8, 12, -3, 7, 6)
+    for (i in result) print(" $i")
+    val numbersA = arrayOf(5, 8, 12, -3, 7, 6)
+    result = sort(numbersA)
+    for (i in result) print(" $i")
 }
+
+fun sort(numbers: MutableList<Int>): List<Int> {
+    for (i in 1 until numbers.size) {
+        for (j in numbers.size - 1 downTo i) {
+            if (numbers[j] < numbers[j - 1]) {
+                val temp = numbers[j]
+                numbers[j] = numbers[j - 1]
+                numbers[j - 1] = temp
+            }
+        }
+    }
+    return numbers
+}
+fun sort(numbers: Array<Int>) = sort(numbers.toMutableList())
+fun sort(vararg numbers: Int) = sort(numbers.toMutableList())
 
 //fun testSimple(x:Int, y:Int): Int {
 //    return x + y
 //}
 fun testSimple(x: Int, y: Int): Int = x + y
+
+fun max(x: Int, y: Int) = if (x > y) x else y
+
+fun crop(str: String) = str.substring(0, Math.min(5, str.length))
+
+fun sum(vararg numbers: Int): Int {
+    var result = 0
+    for (num in numbers) {
+        result += num
+    }
+    return result
+}
 
 //fun testString(x: Int): String {
 //    return "String is $x"
