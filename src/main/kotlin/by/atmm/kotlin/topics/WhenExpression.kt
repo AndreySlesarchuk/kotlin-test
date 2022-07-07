9ccc36dc678d4a7e126d1788f0aa3b59cbf5151f
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2022-07-07 16:04:18 +0300

    WhenExpression.kt - testIf added

--- src/main/kotlin/by/atmm/kotlin/topics/WhenExpression.kt
+++ src/main/kotlin/by/atmm/kotlin/topics/WhenExpression.kt
@@ -10,9 +10,18 @@ fun main() {
     testWhen("Hello")
     testWhen(15)
     testWhen(-1)
+//    One
+//    A string was entered, its length is: 5
+//    From 10 to 20
+//    Something else
+
+    testIf(50)
+//    Do something
+//    You might to buy the following product: Pizza, you still have: 20 dollars
+//    You might to buy the following product: null, you still have: 15 dollars
 }
 
-//testWhen(Odject object)
+// testWhen(Odject object)
 fun testWhen(input: Any) {
     when (input) {
         1 -> println("One")
@@ -21,5 +30,39 @@ fun testWhen(input: Any) {
         is String -> println("A string was entered, its length is: ${input.length} ")
         else -> println("Something else")
     }
+
+}
+
+// testIF
+fun testIf(incomeCount: Any) {
+    var count: Int = incomeCount as Int
+    // food1 - variable type - String
+    val food1 = if (count > 30) {
+        count -= 30
+        println("Do something")
+        "Pizza"
+    } else if (count > 20) {
+        count -= 20
+        "Hamburger"
+    } else {
+        count -= 3
+        "Hot dog"
+    }
+    println("You might to buy the following product: $food1, you still have: $count dollars  ")
+
+    // food2 - variable type - Any
+    val food2 = if (count > 30) {
+        count -= 30
+        println("Do something")
+        "Pizza"
+    } else if (count > 20) {
+        count -= 20
+        300
+    } else {
+        count -= 5
+        null
+    }
+    // println(food2::class.java.typeName)
+    println("You might to buy the following product: $food2, you still have: $count dollars  ")
 
 }
