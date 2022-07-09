8ae05c0ef0372f7e282bed896e78dd948c1f5c30
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2022-07-09 13:28:19 +0300

    Coroutines.kt added to topics

---
+++ src/main/kotlin/by/atmm/kotlin/topics/Coroutines.kt
@@ -0,0 +1,32 @@
+package by.atmm.kotlin.topics
+
+import kotlinx.coroutines.*
+import java.lang.Thread.sleep
+import kotlin.concurrent.thread
+
+/**
+ *  @author Andrey Slesarchuk
+ *  @date 2022-07-09
+ */
+
+
+fun main() {
+    test01()
+    test02()
+}
+
+fun test01() = runBlocking { // this: CoroutineScope
+    launch { // launch a new coroutine and continue
+        delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
+        println(" World!") // print after delay
+    }
+    print("Hello ") // main coroutine continues while a previous one is delayed
+}
+
+fun test02() = runBlocking { // this: CoroutineScope
+    thread { // launch a new coroutine and continue
+        sleep(1000L) // non-blocking delay for 1 second (default time unit is ms)
+        println("World!") // print after delay
+    }
+    print("Hello ") // main coroutine continues while a previous one is delayed
+}
