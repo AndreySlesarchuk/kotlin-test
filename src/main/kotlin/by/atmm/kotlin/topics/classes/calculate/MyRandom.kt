9be9fea011116319d8fd313e55fe27be40b9f890
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2022-09-13 23:40:43 +0300

    code refactoring

--- src/main/kotlin/by/atmm/kotlin/topics/classes/calculate/MyRandom.kt
+++ src/main/kotlin/by/atmm/kotlin/topics/classes/calculate/MyRandom.kt
@@ -16,7 +16,7 @@ class MyRandom {
         private const val SUNDAY = "Sunday"
 
         fun randomInt(from: Int, to: Int) = (Math.random() * (to - from + 1)).toInt() + from
-        fun randomBoolean() = randomInt(0,1) > 0
+        fun randomBoolean() = randomInt(0, 1) > 0
         fun randomDayOfWeek(): String {
             val index = randomInt(1, 7)
             return when (index) {
