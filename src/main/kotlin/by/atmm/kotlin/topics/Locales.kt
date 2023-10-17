b057277888a33393329f1a527459517e1f4bab99
3237573 <3237573@gmail.com>
2023-10-17 14:11:43 +0300

    Locales added

---
+++ src/main/kotlin/by/atmm/kotlin/topics/Locales.kt
@@ -0,0 +1,19 @@
+package by.atmm.kotlin.topics
+
+import java.util.*
+
+/**
+ *  @author Andrei Slesarchuk
+ *  @date 2023-10-17
+ */
+
+fun main() {
+
+    val localeFRANCE = Locale.FRANCE
+    val localeEnglish = Locale.ENGLISH
+    println("Default locale is ${Locale.getDefault()}")
+    println("France locale is $localeFRANCE")
+    println("English locale is $localeEnglish")
+
+}
+
