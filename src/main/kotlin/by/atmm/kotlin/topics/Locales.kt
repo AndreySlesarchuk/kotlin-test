0e44c431f3ab64c6b8920783422d2e6d25387eeb
3237573 <3237573@gmail.com>
2023-10-18 11:54:26 +0300

    Locales changed

--- src/main/kotlin/by/atmm/kotlin/topics/Locales.kt
+++ src/main/kotlin/by/atmm/kotlin/topics/Locales.kt
@@ -11,6 +11,8 @@ fun main() {
 
     val localeFRANCE = Locale.FRANCE
     val localeEnglish = Locale.ENGLISH
+    Locale.setDefault(Locale("ru"))
+
     println("Default locale is ${Locale.getDefault()}")
     println("France locale is $localeFRANCE")
     println("English locale is $localeEnglish")
