027240ef3e5ca1ab3f8a6a608c241f2e56297df6
3237573 <96978266+3237573@users.noreply.github.com>
2025-06-09 10:07:24 +0300

    idioms: add 11-20 points(ext sealed class)

--- src/main/kotlin/by/atmm/kotlin/topics/notes/idioms.kt
+++ src/main/kotlin/by/atmm/kotlin/topics/notes/idioms.kt
@@ -33,24 +33,88 @@ fun main() {
     testString?.let { println(it.uppercase()) } // 9
     val data: String by lazy { dayName as String } // 10
     println(data.uppercase())
-    data class User(val name: String, val age: Int) // 11
-    val user = User(name = testName, age = testAge)
+    val user = User(name = testName, age = testAge) // 11
     println("User is: $user")
+    Database.query("SELECT * FROM user;") // 12
+    println(testName.makeUpperCase()) // 13
+    val totalMoney = Money(13.37) + Money(10.13) // 14
+    println("totalMoney is: ${totalMoney.amount}")
+    println("calculate 14 and 22 is: ${calculate(14, 22, { x, y -> x + y })}") // 15
+    Utils.printMessage() // 16
+    val point2dim = Pair(10, 20)
+    val point3dim = Triple(10, 20, 30)
+    println("point2dim is: $point2dim and point3dim third is: ${point3dim.third}") // 18
+    val (x, y, z) = point3dim // 19
+    println("x is: $x, y is: $y, z is $z")
+    val numbers = listOf(1, 2, 3, 4, 5) // 20
+    val evens = numbers.filter { it % 2 == 0 }
+    println("evens are: $evens")
 
-
+    var messageType: MessageType = msgFailure as MessageType // 17
+    println(getMessage(messageType))
+    messageType = msgSuccess as MessageType
+    println(getMessage(messageType))
 }
 
 // 1. If a function has only one line, you can skip {} and return.
-fun square(n: Int) = n * n
 // 2. You can assign default values to function parameters to avoid overloading methods.
-fun greet(name: String, greeting: String = "Hello") = println("$greeting, $name!")
 // 3. Named arguments make it clear which parameter is which.
 // 4. $variable inside a string automatically replaces with its value.
-// 5. "when" is cleaner and more readable than multiple if-else with count > 2.
+// 5. "When" is cleaner and more readable than multiple if-else with count > 2.
 // 6. If Kotlin knows obj is a String, it automatically lets you use .length.
 // 7.1 ?. → Safe call (calls .length only if name is not null
 // 7.2 ?: → Elvis operator (provides a default value if name is null)
-// 8. apply lets you set properties inside a block instead of writing multiple lines.
-// 9. let executes code only if the value is not null.
+// 8. Apply lets you set properties inside a block instead of writing multiple lines.
+// 9. Let executes code only if the value is not null.
 // 10. Lazy Initialization: by lazy means the variable is only initialized when first accessed.
-// 11. data class automatically provides toString(), equals(), and copy() functions
+// 11. DData class automatically provides toString(), equals(), and copy() functions
+// 12. Object creates a single instance of a class (Singleton).
+// 13. Adds functions to existing classes (even built-in ones).
+// 14. You can override operators like +, -, *, etc.
+// 15. You can pass functions as parameters.
+// 16. Companion object lets you call methods like static functions in Java.
+// 17. Sealed classes restrict inheritance to only within the same file.
+// 18. Pair and Triple help you group values without a class.
+// 19. Destructuring Declarations: val (x, y) = Pair(10, 20) automatically extracts values.
+// 20. Filtering collections: filter selects only matching elements.
+
+fun square(n: Int) = n * n // 1
+
+fun greet(name: String, greeting: String = "Hello") = println("$greeting, $name!") // 2
+
+data class User(val name: String, val age: Int) // 11
+
+object Database { // 12
+    fun query(sql: String) { println("Executing SQL: $sql") }
+}
+
+fun String.makeUpperCase() = this.uppercase() // 13
+
+class Money(val amount: Double) { // 14
+    operator fun plus(other: Money) = Money(this.amount + other.amount)
+}
+
+fun calculate(a: Int, b: Int, op: (Int, Int) -> Int): Int { //15
+    return op(a, b)
+}
+
+class Utils { // 16
+    companion object {
+        fun printMessage() = println("Utils printMessage()")
+    }
+}
+
+sealed class MessageType { //17
+    class Success(var msg: String) : MessageType()
+    class Failure(var msg: String, var e: Exception) : MessageType()
+}
+fun getMessage(messageType: MessageType): String {
+    val msg = when(messageType) {
+        is MessageType.Success -> messageType.msg
+        is MessageType.Failure -> messageType.msg + " " + messageType.e.message
+    }
+    return msg
+}
+
+val msgSuccess = MessageType.Success("Success!") // 17
+val msgFailure = MessageType.Failure("Failure!", Exception("Something went wrong!"))
